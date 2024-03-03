86421b3a0dbc1a82dec820feb9790bd165693769
khasanov-rail <rail.khasanov@gmail.com>
2024-03-04 03:11:32 +0600

    Hw3

--- scrapper/src/main/java/edu/java/scrapper/controller/StackOverflowController.java
+++ scrapper/src/main/java/edu/java/scrapper/controller/StackOverflowController.java
@@ -3,15 +3,19 @@ package edu.java.scrapper.controller;
 import edu.java.scrapper.dto.stackoverflow.Item;
 import edu.java.scrapper.dto.stackoverflow.StackOverflowDTO;
 import edu.java.scrapper.dto.stackoverflow.StackOverflowOwner;
+import java.time.OffsetDateTime;
+import java.util.Collections;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.time.OffsetDateTime;
-import java.util.Collections;
+
 
 @RestController
 @RequestMapping("/api/stackoverflow")
 public class StackOverflowController {
+
+    private static final Long QUESTION_ID_EXAMPLE = 123L;
+    private static final long ANSWER_COUNT_EXAMPLE = 10;
 
     @GetMapping("/questions")
     public StackOverflowDTO getQuestions() {
@@ -19,9 +23,9 @@ public class StackOverflowController {
         Item question = new Item(
             owner,
             OffsetDateTime.now(),
-            123L,
+            QUESTION_ID_EXAMPLE,
             "Пример заголовка вопроса",
-            10
+            ANSWER_COUNT_EXAMPLE
         );
         StackOverflowDTO response = new StackOverflowDTO(Collections.singletonList(question));
 
