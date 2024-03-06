670649e35d85f0877ca5fa7e7ba6d1602c49ae5c
khasanov-rail <rail.khasanov@gmail.com>
2024-03-07 02:24:10 +0600

    Hw3 WireMockBaseTest

--- scrapper/src/test/java/edu/java/scrapper/clients/WireMockBaseTest.java
+++ scrapper/src/test/java/edu/java/scrapper/clients/WireMockBaseTest.java
@@ -2,26 +2,37 @@ package edu.java.scrapper.clients;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.web.reactive.function.client.WebClient;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
 public abstract class WireMockBaseTest {
 
-    protected WireMockServer wireMockServer;
+    protected static WireMockServer wireMockServer;
     protected WebClient webClient;
 
-    @BeforeEach
-    void setup() {
+    @BeforeAll
+    static void globalSetup() {
         wireMockServer = new WireMockServer(wireMockConfig().dynamicPort());
         wireMockServer.start();
         WireMock.configureFor("localhost", wireMockServer.port());
+    }
+
+    @BeforeEach
+    void setup() {
         webClient = WebClient.builder().baseUrl(wireMockServer.baseUrl()).build();
     }
 
     @AfterEach
     void tearDown() {
+        WireMock.resetAllRequests();
+    }
+
+    @AfterAll
+    static void globalTearDown() {
         wireMockServer.stop();
     }
 }
