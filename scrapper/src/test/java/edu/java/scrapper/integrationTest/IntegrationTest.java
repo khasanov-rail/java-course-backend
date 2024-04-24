566944a5079bd7e45458e2e0ca2e24b51b2aee7e
khasanov-rail <rail.khasanov@gmail.com>
2024-04-24 09:48:48 +0500

    Hw9+hw6

--- scrapper/src/test/java/edu/java/scrapper/integrationTest/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/integrationTest/IntegrationTest.java
@@ -4,20 +4,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static edu.java.scrapper.integrationTest.IntegrationEnvironment.POSTGRES;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class IntegrationTest {
 
     @Test
     @DisplayName("Проверка соединения с базой данных")
     public void testConnection() {
-        Assertions.assertTrue(POSTGRES.isRunning(), "Контейнер с базой данных должен быть запущен.");
-        assertThat(POSTGRES.getUsername()).as("Имя пользователя базы данных должно быть 'postgres'")
+        Assertions.assertTrue(
+            IntegrationEnvironment.POSTGRES.isRunning(),
+            "Контейнер с базой данных должен быть запущен."
+        );
+        org.assertj.core.api.Assertions.assertThat(IntegrationEnvironment.POSTGRES.getUsername())
+            .as("Имя пользователя базы данных должно быть 'postgres'")
             .isEqualTo("postgres");
-        assertThat(POSTGRES.getPassword()).as("Пароль пользователя базы данных должен быть 'postgres'")
+        org.assertj.core.api.Assertions.assertThat(IntegrationEnvironment.POSTGRES.getPassword())
+            .as("Пароль пользователя базы данных должен быть 'postgres'")
             .isEqualTo("postgres");
-        assertThat(POSTGRES.getDatabaseName()).as("Имя базы данных должно быть 'scrapper'").isEqualTo("scrapper");
+        org.assertj.core.api.Assertions.assertThat(IntegrationEnvironment.POSTGRES.getDatabaseName())
+            .as("Имя базы данных должно быть 'scrapper'").isEqualTo("scrapper");
     }
 }
